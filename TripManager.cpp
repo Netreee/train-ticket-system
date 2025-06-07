459187bd0e2047b11389a07620eb8125ebb1036a
feshabyssal <3265244311@qq.com>
2025-06-07 12:18:54 +0800

    Update TripManager.cpp

--- TripManager.cpp
+++ TripManager.cpp
@@ -8,13 +8,16 @@ namespace trainsys {
 
     void TripManager::addTrip(const UserID &userID, const TripInfo &trip) {
         /* Question */
+        tripInfo.insert(userID, trip);
     }
 
     seqList<TripInfo> TripManager::queryTrip(const UserID &userID) {
         /* Question */
+        return tripInfo.find(userID);
     }
 
     void TripManager::removeTrip(const UserID &userID, const TripInfo &trip) {
         /* Question */
+        tripInfo.remove(userID, trip);
     }
 }
